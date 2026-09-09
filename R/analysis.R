df899edf0de2670e02b4d6aed60f23d61bb2c66c
IzGut <israel.gutierrez@graduateinstitute.ch>
2026-09-09 16:13:30 +0200

    Add revenue bar chart

--- R/analysis.R
+++ R/analysis.R
@@ -6,3 +6,10 @@ total_revenue <- sum(sales$revenue)
 
 print(sales)
 print(total_revenue)
+
+barplot(
+  sales$revenue,
+  names.arg = sales$product,
+  main = "Revenue by Transaction",
+  ylab = "Revenue"
+)
