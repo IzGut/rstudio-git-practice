421cc80484c1dfc4fb4b22a5346a7f1e56b67c87
IzGut <israel.gutierrez@graduateinstitute.ch>
2026-09-16 12:16:43 +0200

    Import the data

--- R/analysis.R
+++ R/analysis.R
@@ -1,15 +1,1 @@
 sales <- read.csv("data/sales.csv")
-
-sales$revenue <- sales$quantity * sales$price
-
-total_revenue <- sum(sales$revenue)
-
-print(sales)
-print(total_revenue)
-
-barplot(
-  sales$revenue,
-  names.arg = sales$product,
-  main = "Revenue by Transaction",
-  ylab = "Revenue"
-)
